2034847298d4e2de384d492160d0400abedd619a
alisonpSWE <alisonp.dev@gmail.com>
2026-04-20 17:47:20 -0300

    pagina inicio

---
+++ frontend/src/pages/Inicio.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Inicio() {
+  return (
+    <div className="mt-4">
+      <h1 className="poppins-bold">Sistema Integrado da Mineradora</h1>
+      <p className="poppins-regular mt-3">Bem-vindo ao painel de controle. Utilize o menu acima para gerenciar os recursos.</p>
+    </div>
+  );
+}

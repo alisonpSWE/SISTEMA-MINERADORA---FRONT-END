ab42728ac888317e0558ec18f1933ea5f1250635
alisonpSWE <alisonp.dev@gmail.com>
2026-05-11 11:02:06 -0300

    estrutura base da api

---
+++ frontend/src/services/api.js
@@ -0,0 +1,15 @@
+import supabase from './supabase';
+
+export const equipamentoService = {
+  listar: async () => {
+    // TODO: implement
+    return { data: [] };
+  }
+};
+
+export const funcionarioService = {
+  listar: async () => {
+    // TODO: implement
+    return { data: [] };
+  }
+};

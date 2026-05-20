977afdf7d8177d32cfbe4961820a6d3a40a1545a
alisonpSWE <alisonp.dev@gmail.com>
2026-05-20 12:16:50 -0300

    pagina funcionarios

---
+++ frontend/src/pages/Funcionarios.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Funcionarios = () => {
+  return (
+    <div>
+      <h1>Funcionários</h1>
+      <p>Página em construção...</p>
+    </div>
+  );
+};
+
+export default Funcionarios;

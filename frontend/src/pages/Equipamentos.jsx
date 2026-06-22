b1a680f4cfec20ca5e25f14a2b9f602fe50eafd6
alisonpSWE <alisonp.dev@gmail.com>
2026-06-22 11:00:10 -0300

    pagina equipamentos e componentes

---
+++ frontend/src/pages/Equipamentos.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Equipamento from '../components/Equipamento';
+
+const Equipamentos = () => {
+  return (
+    <div>
+      <h1>Equipamentos</h1>
+      {/* TODO: Listar equipamentos aqui */}
+    </div>
+  );
+};
+
+export default Equipamentos;
